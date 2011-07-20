10d02f0d2db101b68f1ab7cf36db83f35449e6b1
lazzalf <lazzalf@localhost>
2011-07-20 16:36:36 +0200

    Bitmatrix * Aggiornato File di Test Main * Small fix

--- src/bitmatrix/bitmatrix_test.cpp
+++ src/bitmatrix/bitmatrix_test.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 int main()
 {
-    bitmatrix b1(3,3),b2(3,3);
-    string s;
+    bitmatrix b1(4,4),b2(5,5);
+    string s("00110010");
     
     srand(time(NULL));
-    
+   /* 
     b1.Set(0,0);
     b1.Unset(0,1);
     b1.Set(0,2);
@@ -32,18 +32,28 @@ int main()
     printf("\n");
     
     b2.FlipAll();
-    b2.Print();
+    b2.Print();*/
     
-    printf("\n");
-        
+    printf("\n");        
     b2.RandomizeAll();
-    b2.RandomizeAll();
+    b1.RandomizeAll();
     b2.Print();
-    
     printf("\n");
+    b1.Print();
+    printf("\n");
+    b2.Import(b1);
+    b2.Print();    
+    printf("\n");
+
+    b2.StringToCol(s, 0);
     
-    s = b2.ToString();
     printf("%s\n",s.c_str());
+    printf("\n");
+    b2.Print();    
+    printf("\n");
+    
+    // s = b2.ToString();
+    // printf("%s\n",s.c_str());
     
     return 0;
 }
