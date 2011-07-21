534264fa370c3b41cd444d011e22cb58ef0466a9
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2011-07-21 14:17:50 +0200

    update config per supportare le classi della cartella simulation

--- src/config.cpp
+++ src/config.cpp
@@ -15,11 +15,18 @@ bool load_config(string conf_filename, config *c)
         CFG_BOOL((char*)"print_kills",(cfg_bool_t)false, CFGF_NONE),
         CFG_BOOL((char*)"print_mutations",(cfg_bool_t)false, CFGF_NONE),
 
+        CFG_STR((char*)"simulator_dir",(char*)"",CFGF_NONE),
+        CFG_STR((char*)"simulator_bin",(char*)"",CFGF_NONE),
+        CFG_STR((char*)"simulator_patch",(char*)"simulator.patch",CFGF_NONE),
+        CFG_STR((char*)"test_file_outpath",(char*)"",CFGF_NONE),
+        CFG_STR((char*)"test_file_inpath",(char*)"",CFGF_NONE),
+
         CFG_INT((char*)"avg_population_size", 4096, CFGF_NONE),
         CFG_INT((char*)"max_iterations",  8192, CFGF_NONE),
         CFG_INT((char*)"dna_length", 15 , CFGF_NONE),
         CFG_INT((char*)"chromosome_length",  10, CFGF_NONE),        
         CFG_INT((char*)"max_stall", 100, CFGF_NONE),
+        
         CFG_FLOAT((char*)"mutation_rate", 0.06f, CFGF_NONE),
         CFG_FLOAT((char*)"mutation_strength", 2.0f, CFGF_NONE),
         CFG_FLOAT((char*)"mate_rate", 0.1f, CFGF_NONE),
@@ -42,6 +49,13 @@ bool load_config(string conf_filename, config *c)
     c->print_population_size = cfg_getbool(cfg, "print_population_size");
     c->print_kills = cfg_getbool(cfg, "print_kills");
     c->print_mutations = cfg_getbool(cfg, "print_mutations");
+
+    c->simulator_dir = cfg_getstr(cfg, "simulator_dir");
+    c->simulator_bin = cfg_getstr(cfg, "simulator_bin");
+    c->simulator_patch = cfg_getstr(cfg, "simulator_patch");
+    c->test_file_outpath = cfg_getstr(cfg, "test_file_outpath");
+    c->test_file_inpath = cfg_getstr(cfg, "test_file_inpath");
+
 
     c->avg_population_size = cfg_getint(cfg, "avg_population_size");
     c->max_iterations = cfg_getint(cfg, "max_iterations");
