ba02b7342bcebecc7fa16e8f066c2deccca58eab
Lazzalf <Lazzalf@localhost>
2011-07-22 12:33:31 +0200

    Tests * Inserite varie funzioni membro base

--- src/simulation/tests.cpp
+++ src/simulation/tests.cpp
@@ -1,4 +1,4 @@
-#include"tests.h"
+#include "tests.h"
 
 tests::tests()
 {
@@ -9,3 +9,47 @@ tests::~tests()
 {
 
 }
+
+uint32 tests::GetFaults()
+{
+    return m_faults_set.size();
+}
+
+/*
+set<uint32> const& GetFaultsList() const
+{
+    return m_faults_set;
+}
+*/
+
+bool tests::DeleteFault(uint32 fault)
+{
+    set<uint32>::iterator itr = m_faults_set.find(fault);
+    if (itr == m_faults_set.end())
+        return false;
+
+    m_faults_set.erase(itr);
+
+    return true;
+}    
+
+bool tests::InsertFault(uint32 fault)
+{
+    set<uint32>::iterator itr = m_faults_set.find(fault);
+    if (itr != m_faults_set.end())
+        return false;
+
+    m_faults_set.insert(fault);
+
+    return true;
+}  
+
+bool tests::FindFault(uint32 fault)
+{
+    set<uint32>::iterator itr = m_faults_set.find(fault);
+    if (itr == m_faults_set.end())
+        return false;
+
+    return true;
+}                    
+
