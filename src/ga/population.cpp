1959d497e96e0f4bbb2460e0c71140274788ae19
Lazzalf <Lazzalf@localhost>
2011-07-23 15:18:17 +0200

    Population * Fix funzione geet_best_fitness() * Convertita classe pool in puntatore * Inserita create_mating_pool()

--- src/ga/population.cpp
+++ src/ga/population.cpp
@@ -19,25 +19,29 @@ population::population()
         cout << "chromosome_start_len_min value must be lower than chromosome_start_len_max value" << endl;
         exit(0);
     }
+
+    pool = new individual_map;
 }
 
 population::~population()
 {
-    for (individual_map::iterator it = pool.begin(); it != pool.end(); it++)
+    for (individual_map::iterator it = pool->begin(); it != pool->end(); it++)
     {
         if ((*it).second) 
             delete (*it).second;
     }
+
+    delete pool;
 }
 
 individual* population::get_random_individual() const
 {
     individual *i = NULL;
-    int rnd = rand()%pool.size() + 1, count = 0;
+    int rnd = rand()%pool->size() + 1, count = 0;
     
     
-    individual_map::const_iterator it = pool.begin();
-    for (it = pool.begin(); it!=pool.end(); ++it)
+    individual_map::const_iterator it = pool->begin();
+    for (it = pool->begin(); it != pool->end(); ++it)
     {
         if (count == rnd)
         {
@@ -70,16 +74,16 @@ void population::new_random_population()
     int created = 0;
 
     while (created++ < conf->population_size)
-        pool.insert(pool.end(), \
+        pool->insert(pool->end(), \
                     individual_pair(created, new_random_individual()));
 
 }
 
 void population::calc_fitness()
 {
-    individual_map::const_iterator it = pool.begin();
+    individual_map::const_iterator it = pool->begin();
     
-    for (it = pool.begin(); it!=pool.end(); ++it)
+    for (it = pool->begin(); it != pool->end(); ++it)
     {
         (*it).second->calc_fitness();
     }  
@@ -88,46 +92,53 @@ void population::calc_fitness()
 float population::get_avg_fitness() const
 {
     float sum_fitness = 0;
-    individual_map::const_iterator it = pool.begin();
+    individual_map::const_iterator it = pool->begin();
     
-    for (it = pool.begin(); it!=pool.end(); ++it)
+    for (it = pool->begin(); it != pool->end(); ++it)
     {
         sum_fitness+=(*it).second->get_fitness();
     }  
 
-    if (pool.size() == 0)
+    if (pool->size() == 0)
         return 0;
     else 
-        return sum_fitness/pool.size();
+        return sum_fitness/pool->size();
 }
 
 float population::get_best_fitness() const
 {
-    individual_map::const_iterator it = pool.begin();
-    return (float)(*it).second->get_fitness();
+    float best_fitness = 0;
+
+    individual_map::const_iterator it = pool->begin();
+    for (; it != pool->end(); ++it)
+    {
+        if (best_fitness < (*it).second->get_fitness())
+            best_fitness = (*it).second->get_fitness();
+    }
+    return best_fitness;
 }
 
 void population::sort_by_fitness()
 {
-    //pool.sort();
+    //pool->sort();
 }
 
 void population::mate_individuals()
 {
     for (int i=0; i<(rand()%100+1); i++) // temporaneo per testare kill_individuals
     {
-        pool.insert(pool.end(), \
-                    individual_pair(pool.size(), new_random_individual()));
+        pool->insert(pool->end(), \
+                    individual_pair(pool->size(), new_random_individual()));
     }
 }
 
 void population::mutate_individuals() const
 {
-    individual_map::const_iterator it = pool.begin();
     float mutate_probability = conf->mutation_rate*100;
     float rnd;
-    
-    for (it = pool.begin(); it!=pool.end(); ++it)
+
+    individual_map::const_iterator it = pool->begin();
+    for (; it != pool->end(); ++it)
     {
         rnd = rand()%100 + 1; 
         
@@ -143,73 +154,76 @@ void population::mutate_individuals() const
 
 void population::kill_individuals(uint32 howmany)
 {
-    individual_map::iterator it = pool.begin();
-    if (pool.size() > conf->population_size)
+    individual_map::iterator it = pool->begin();
+    if (pool->size() > conf->population_size)
     {
         if (conf->verbose && conf->print_kills)
-            cout << "killing: " << (pool.size() - conf->population_size) << endl;
+            cout << "killing: " << (pool->size() - conf->population_size) << endl;
         
         advance(it, conf->population_size);
-        pool.erase(it, pool.end());
+        pool->erase(it, pool->end());
     }
 }
 
 uint32  population::size() const
 {
-    return pool.size();
+    return pool->size();
 }
 
 void population::create_mating_pool()
 {
-/*
     weight_map m_weight_map;
     uint32 total_weight = 0;
-    uint32 selected_weight = 0;
-    uint32 id = 0;
-    uint32 weight;
-    
-    // inserisci i vari elementi nella mappa con la fitness o un numero dipendente da essa
-    m_weight_map[id] = selected_weight;
-     
-    // se non ci sono elementi
-    if (m_weight_map.size())
-      return;
 
-    // Somma tutti i vari pesi (può essere anche calcolato prima per ottimizzare quando viene aggiunto alla mappa)
-    for (weight_map::const_iterator it = m_weight_map.begin(); it != m_weight_map.end(); ++it)
-        total_weight += it->second;
+    mating_pool.clear();
 
-    // Peso totale 0 si ritorna, non si sa mai
-    if (!total_weight)
-        return;
-        
-    selected_weight = rand(0, weight-1);
-    weight = 0;
-    for (weight_map::const_iterator it = m_weight_map.begin(); it != m_weight_map.end(); ++it)
+    individual_map::const_iterator it = pool->begin();
+    for (; it != pool->end(); ++it)
     {
-        weight += it->second;
-        if (selected_weight < weight)
-        {
-            id = it->first;
-            break;
-        }
-    }  
-*/ 
+        uint32 u_fitness = uint32((*it).second->get_fitness() * 1000);
+        if (!u_fitness)
+            continue;
+  
+        total_weight += u_fitness;
+        m_weight_map[(*it).first] = u_fitness;
+    }
+
+    if (m_weight_map.size())
+        return;   
+   
+    uint32 selected_weight;
+    uint32 weight;
+
+    for (uint32 i = 0; i < conf->population_size; i++)
+    {
+         selected_weight = rand()%total_weight;
+         weight = 0;
+
+         for (weight_map::const_iterator it = m_weight_map.begin(); it != m_weight_map.end(); ++it)
+         {
+             weight += it->second;
+             if (selected_weight < weight)
+             {
+                mating_pool.push_front((*it).first);
+                break;
+             }
+         }
+    }
 }
 
 void population::print_best() const
 {
-    individual_map::const_iterator it = pool.begin();
+    individual_map::const_iterator it = pool->begin();
     cout << (*it).second->get_dna();
 }
 
 void population::print_all(string logfile) const
 {
-    individual_map::const_iterator it = pool.begin();
+    individual_map::const_iterator it = pool->begin();
     uint32 count = 0;
     string str;
 
-    for (it = pool.begin(); it!=pool.end(); ++it)
+    for (it = pool->begin(); it != pool->end(); ++it)
     {   
         str+= "individual: #";
         str+= count; 
