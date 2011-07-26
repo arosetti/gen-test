c313cc6624146362f0df00176cbbe6707e0a5f38
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2011-07-26 19:26:57 +0200

    inizio dei lavori sulla classe simulation

--- src/simulation/simulation.cpp
+++ src/simulation/simulation.cpp
@@ -9,3 +9,22 @@ simulation::~simulation()
 {
 
 }
+
+bool simulation::execute_simulation(string dna)
+{
+    string path = conf->simulator_dir;
+    if (path.c_str()[path.length()] != "/")
+        path += "/";
+    path+= conf->simulator_bin;
+
+    if(!file_exist(path))
+    {
+        cout << "can't find simulator binary." << endl;
+        exit(0);
+    }
+
+    path += " ";
+    path += conf->simulator_opts;
+    system("");
+}
+
--- src/simulation/simulation.h
+++ src/simulation/simulation.h
@@ -1,20 +1,26 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
+#include <unistd.h>
 #include <cstdlib>
 #include <string>
 
+#include "../common/utility.h"
+
 using namespace std;
+
+extern config *conf;
 
 class simulation
 {
-    string simulator_dir;
-    string simulator_bin;
-
+    void write_simulation_input();
+    
     public:
     simulation();
     ~simulation();
-
+   
+    void rebuild_simulator();
+    void execute_simulation(string);
 };
 
 #endif
