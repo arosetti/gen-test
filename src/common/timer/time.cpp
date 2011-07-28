e57c8ce79b6337e62ee14e37634f485dd1f28f83
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2011-07-28 20:26:07 +0200

    inserita funzione per l'output dei secondi in formato   year day hour min sec

--- src/common/timer/time.cpp
+++ src/common/timer/time.cpp
@@ -19,3 +19,51 @@ void time_stop(timer &time)
 {
     gettimeofday(&time.stop,NULL);
 }
+
+string time_format(uint32 sec)
+{
+    stringstream out;
+
+    if (sec < 60)
+        out << sec << " second(s)"; 
+    else if (sec < 3600)
+    {
+        uint32 min = (uint32)sec/60;
+        uint32 sec_res = (uint32)sec%60;
+        out << min << " minute(s), " << sec_res << " second(s)"; 
+    }
+    else if ( sec < 86400 )
+    {
+        uint32 hours = (uint32) sec/3600;
+        uint32 hour_res = (uint32)sec%3600;
+        uint32 min = (uint32)hour_res/60;
+        uint32 sec= (uint32)sec%60;
+        out << hours << " hour(s), " << min << " minute(s), " << sec << " second(s)"; 
+    }
+    else if ( sec < 31536000 )
+    {
+        uint32 days = (uint32) sec/86400;
+        uint32 days_res = (uint32)sec%86400;
+        uint32 hours = (uint32) days_res/3600;
+        uint32 hour_res = (uint32)(sec - 86400)%3600;
+        uint32 min = (uint32)hour_res/60;
+        uint32 sec= (uint32)sec%60;
+        
+        out << days<<" day(s), "<<hours<< " hour(s), "<< min<< " minute(s), "<<sec<< " second(s)"; 
+    }
+    else
+    {
+        uint32 years = (uint32) sec/31536000;
+        uint32 years_res = (uint32) sec%31536000;
+        uint32 days = (uint32) years_res/86400;
+        uint32 days_res = (uint32)sec%86400;
+        uint32 hours = (uint32) days_res/3600;
+        uint32 hour_res = (uint32)(sec - 86400)%3600;
+        uint32 min = (uint32)hour_res/60;
+        uint32 sec= (uint32)sec%60;
+        out << years << " year(s), " << days << " day(s), " << hours << " hour(s), " << min << " minute(s), " << sec << " second(s)"; 
+    }
+
+    return out.str();
+}
+
