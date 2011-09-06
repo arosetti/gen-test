04d7a9a0e0a07c10e0d700f9c48a402816cf1cc7
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2011-09-06 11:32:19 +0200

    gestione parametri thread e inizializzazione cartelle di simulazione aggiunta addslash()

--- src/simulation/simulation.h
+++ src/simulation/simulation.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <cstdio>
 //#include <sys/types.h>
 //#include <regex.h>
 
@@ -27,6 +28,8 @@ class simulation
 
     string get_sim_path();
     string get_bin_path();
+    string get_outputnet_path();
+    string get_faults_path();
     string get_patch_path();
     string get_input_file_path();
     string get_output_file_path();
