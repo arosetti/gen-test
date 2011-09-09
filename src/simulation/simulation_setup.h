6d36ed6dbf0d11e54caa13c9f306879441c69d7d
Lazzalf <Lazzalf@localhost>
2011-09-09 22:37:06 +0200

    Check in set_mutation_rate

---
+++ src/simulation/simulation_setup.h
@@ -0,0 +1,30 @@
+#ifndef SIMULATION_SETUP_H
+#define SIMULATION_SETUP_H
+
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+//#include <sys/types.h>
+//#include <regex.h>
+
+#include "../config.h"
+#include "../bitmatrix/bitmatrix.h"
+
+extern string dir_path;
+
+string get_sim_path(int);
+string get_bin_path(int);
+string get_outputnet_path(int);
+string get_faults_path(int);
+string get_patch_path(int);
+string get_input_file_path(int);
+string get_output_file_path(int);
+
+void clean_env();
+void init_env();
+
+uint32 read_n_inputs();
+
+#endif
