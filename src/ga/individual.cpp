04e2790eab0abe5ee18f87f9c6b8a76740f1a6f1
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2011-09-09 22:32:44 +0200

    fix mutation

--- src/ga/individual.cpp
+++ src/ga/individual.cpp
@@ -76,7 +76,7 @@ void individual::dna_mutate(float mutation_rate)
 {
     uint32 row_r = rand()%chromosome_number + 1,
            col_r = rand()%get_chromosome_length() + 1;
-    float count = 100 * mutation_rate * dna_length();
+    float count = mutation_rate * dna_length();
 
     if (conf->verbose && conf->print_mutations)
     {
