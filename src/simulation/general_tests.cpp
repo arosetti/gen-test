2b674ad5faaa201af92d14a38543db60e658e58a
Lazzalf <Lazzalf@localhost>
2011-09-10 12:40:54 +0200

    Test * Modificato parser dei Fault

--- src/simulation/general_tests.cpp
+++ src/simulation/general_tests.cpp
@@ -59,7 +59,7 @@ uint32 general_tests::FindFault(uint32 fault)
     return m_faults_map[fault];
 }
 
-// Da splittare nei singoli individui (ci sto lavorando XD)
+// Deprecata (ma può servire in futuro)
 bool general_tests::LoadFile(individual_map* pool)
 {
     string path = conf->simulator_dir;
