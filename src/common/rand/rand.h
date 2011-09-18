0d8480c857555a229a7f7427644a24fea0af8222
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2011-09-18 22:02:43 +0200

    * aggiunta rand_gaussian * aggiunto sistema di test del random * permessi di tutti i file scavolati per bene

--- src/common/rand/rand.h
+++ src/common/rand/rand.h
@@ -1,17 +1,18 @@
-#include <unistd.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <sys/time.h>
 #include <time.h>
 #include <limits.h>
+#include <unistd.h>
+#include <math.h>
 
 #ifndef RAND_H
 #define RAND_H
 
 #include "../typedefs.h"
 
+void     init_rand_seed();
 unsigned time_seed();
 uint32   randmm(uint32, uint32);
 bool     randb();
-void     init_rand_seed();
-
+double   rand_gaussian(double, double);
 #endif
