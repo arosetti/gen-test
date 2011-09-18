bacbe81a31aad4deaf0f51318cef23b31a09a5b5
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2011-09-18 22:22:45 +0200

    * fix bug population mate_individuals, l'ultimo invididuo non veniva mutato   come gli altri che non si accoppiano. * hack temporaneo inserimento mutazione gaussiana   (pura curiosit? dopo lo reverto se non fa nulla)

--- src/common/rand/rand_test.cpp
+++ src/common/rand/rand_test.cpp
@@ -1,6 +1,37 @@
 #include <cstdio>
 
 #include "rand.h"
+/* mean m, standard deviation s */
+
+/*
+float gauss(float m, float s)
+{
+    float x1, x2, w, y1;
+    static float y2;
+    static int use_last = 0;
+
+    if (use_last)
+    {
+        y1 = y2;
+        use_last = 0;
+    }
+    else
+    {
+        do {
+            x1 = 2.0 * drand48() - 1.0;
+            x2 = 2.0 * drand48() - 1.0;
+            w = x1 * x1 + x2 * x2;
+        } while ( w >= 1.0 );
+
+        w = sqrt( (-2.0 * log( w ) ) / w );
+        y1 = x1 * w;
+        y2 = x2 * w;
+        use_last = 1;
+    }
+
+    return( m + y1 * s );
+}
+*/
 
 int main()
 {
